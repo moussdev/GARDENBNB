dd1c6b02e491f123217690cb7009c297ae4188fa
robinmagot <magot.robin@gmail.com>
2021-10-01 04:31:31 +0200

    Seeds+Models+routes+views

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,14 @@
 class Garden < ApplicationRecord
   belongs_to :user
   has_many :reservations
+
+  LIST_OF_LAND = ['rocheux', 'argileux', 'sablonneux', 'terre agricole']
+
+  validates :title, :price, :image, :description, :address, :land, presence: true
+  validates :title, length: { minimum: 6, maximum: 25 }
+  validates :description, length: { minimum: 25, maximum: 250 }
+  validates :address, length: { minimum: 10, maximum: 70 }
+  validates :price, numericality: { only_integer: true }
+  
+  validates :land, inclusion: { in: LIST_OF_LAND }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_232604) do
+ActiveRecord::Schema.define(version: 2021_09_30_192223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_232604) do
     t.string "title"
     t.string "image"
     t.string "address"
-    t.string "type"
+    t.string "land"
     t.integer "price"
     t.text "description"
     t.bigint "user_id", null: false
