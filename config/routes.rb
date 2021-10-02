324ee2d9d69c450c42d3d7dfd7ae1fb543784000
agouram mustapha <agouram.mustapha@gmail.com>
2021-10-02 16:19:32 +0200

    navabr

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'gardens#index'
   resources :gardens, only: [:new, :show, :create]
+  get "about_us", to: "pages#about_us"
   #get '/post', to: 'pages#register'
   #get '/reservations', to: 'pages#reservations'
   #get '/dashboard', to: 'pages#dashboard'
