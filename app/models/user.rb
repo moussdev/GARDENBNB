1b5439ae0a0a7fc8a7f568d0f383e5d9e6f45aec
robinmagot <magot.robin@gmail.com>
2021-10-09 12:59:45 +0200

    ProfilePics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_many :gardens
+  has_many :gardens, dependent: :destroy
   has_many :reservations
+  
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
